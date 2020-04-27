ce82ea9360b8314cf7714e328f14b9920db1d6d8
leeshanex <leeshanex@outlook.com>
2020-04-27 08:48:55 -0700

    UnitTest project created, CustomList class added

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using __________________________;
+
+
+namespace Custom_List_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
