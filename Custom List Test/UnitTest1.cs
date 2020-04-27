051607d47aa752e58f8c85e18e912be47c31be77
leeshanex <leeshanex@outlook.com>
2020-04-27 12:41:53 -0700

    Starter code implemented

--- Custom List Test/UnitTest1.cs
+++ Custom List Test/UnitTest1.cs
@@ -8,8 +8,60 @@ namespace Custom_List_Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_AddingOneValueToEmptyCustomList_AddedValueGoesToIndexZero()
         {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 25;
+            int expected = 25;
+            int actual;
+
+            //Act
+            testList.Add(itemToAdd);
+            actual = testList[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }  
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_CountOfCustomListIncrements()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 1;
+            int actual;
+
+            //Act
+            testList.Add(itemToAdd);
+            actual = testList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        } 
+        public void Add_AddingMultipleValueToEmptyCustomList_()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 1;
+            int actual;
+
+            //Act
+            testList.Add(itemToAdd);
+            actual = testList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
-    }
+        public void Add_AddingMultipleValueToEmptyCustomList_IncreasingCapacityCount()
+        {
+            //Arrange
+            int itemToAdd = 
+            //Act
+
+            //Assert
+        }
+       
+    } 
 }
--- CustomList.cs
+++ CustomList.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Custom_List_Proj
 {
-    class CustomList
+    class CustomList<T>
     {
+        //member variables
+        private T[] items;
+        //constructor
+        public CustomList()
+        {
+            items = new T[4];
+        }
+
+        //member methods
+        public void Add(T item)
+        {
+
+        }
+
     }
 }
