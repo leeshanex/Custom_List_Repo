99fddc8e677285a14da36a7789cd1dc876d09cea
leeshanex <leeshanex@outlook.com>
2020-04-29 09:09:19 -0700

    Indexer added

--- CustomList.cs
+++ CustomList.cs
@@ -13,31 +13,38 @@ namespace Custom_List_Proj
         private int count;
         private int capacity;
         private int remove;
+        public T this[int index]
+        {
+            get
+            {
+                if(index < count && index >= 0)
+                {
+                    return items[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
         //constructor
         public CustomList()
         {
             items = new T[4];
+            count = 0;
+            capacity = 4;
         }
 
         //member methods
+
         public void Add(T item)
         {
-                                                       
+            items[count] = item;
+            count++;
         }
-        public void Count()
-        {
-
-        }
-        public void Capacity()
-        {
-
-        }
-        public void Remove()
-        {
-
-        }
-
-    
-
     }
 }
